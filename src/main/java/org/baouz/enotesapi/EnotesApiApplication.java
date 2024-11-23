c99a7b254a99d049b5897712e8d5206b00e96983
Sifeddine <sifeddinebaouz@gmail.com>
2024-11-23 19:04:47 +0100

    Initial commit

---
+++ src/main/java/org/baouz/enotesapi/EnotesApiApplication.java
@@ -0,0 +1,13 @@
+package org.baouz.enotesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnotesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/baouz/enotesapi/EnotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baouz.enotesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
